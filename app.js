cd1868f55ab0a6cdde8d0b516a7ec3b0543481d1
mrmatteorusso <mrmatteorusso@gmail.com>
2021-10-03 12:53:36 +0200

    removingall function and counting items with bug

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const clearAll = document.querySelector('.bottomForm');
+const containerList = document.querySelector('.containerList');
 // salvato gli items dal local storage in una variabile
 let storedItems = localStorage.getItem('tasks');
 
@@ -9,7 +11,7 @@ const generateTemplate = todo => {
         <div class="tick">
             <input type="checkbox" id="" name="" value="">
         </div>
-        <div class="content delete">
+        <div class="content">
             <span>${todo}</span>
             <i class="fas fa-times delete"></i>
         </div>
@@ -37,21 +39,73 @@ addForm.addEventListener('submit', e => {
         addForm.reset();
         console.log(`${todo} has been added to html list`)
         console.log(`Local storage now contains ${storedItems}`)
+        document.getElementById("numberItems").innerHTML = `${storedItems.length} item(s) `; //counting element when added
+        console.log(`aftr adding, now the items are ${storedItems.length}`)
 
     }
 });
 
 /*Removing item*/
-list.addEventListener('click', e => {
+containerList.addEventListener('click', e => {
     console.log(e.target);
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.parentElement.remove();
         let removedItem = e.target.parentElement.firstElementChild.innerText;
-        console.log(`${removedItem} has been removed from the html list`);
-        console.log(storedItems)
+        //console.log(`${removedItem} has been removed from the html list`);
+        //console.dir(e.target.parentElement.firstElementChild.innerText);
+        //console.log(storedItems)
         const newArr = storedItems.filter(item => item !== removedItem)
-        console.log(newArr)
+        //console.log(newArr)
         storedItems = newArr
-        console.log(`Local storage now contains ${storedItems} `)
+        localStorage.setItem('tasks', JSON.stringify(storedItems))
+        document.getElementById("numberItems").innerHTML = `${storedItems.length} item(s) `; //counting element when deleted
+        //console.log(`Local storage now contains ${storedItems} `)
+        //console.log(`after removing, now the items are ${storedItems.length}`)
     }
+    if (e.target.classList.contains('clears')){
+
+        window.localStorage.removeItem('tasks');
+        console.log('clear button has been pushed')
+        //document.getElementsByClassName("newList").innerHTML = ''
+        // for (const child of list.children) {
+        //     child.remove()
+        //   }
+    
+        for (let i= list.childNodes.length -1; i>=0; i--) {
+            list.childNodes[i].remove()
+            //list.removeChild(list.lastChild)
+        }
+        // const storeArray = [...list.children].forEach(child => child.remove())
+        // storedItems = storeArray;
+    }
+    
+    
+    
 })
+
+
+
+/*buttom list functions*/
+
+// function deleteAll() {
+//     //list.removeAllChildNodes();
+//     // document.getElementsByClassName("newList").innerHTML = 'ciao'
+//     // console.log('it has been clicked');
+//    //document.getElementsByTagName('li').remove()
+//    console.log(list.remove())
+
+// }
+
+function myFunction() {
+
+    console.log('it has been clicked');
+}
+
+// clearAll.addEventListener('click', e => {
+// console.log(e.target);
+// if (e.target.classList.contains('clears')){
+//     //list.children.remove();
+//     window.localStorage.removeItem('tasks');
+//     console.log('clear button has been pushed')
+// }
+// })
